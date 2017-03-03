e1359b0e609ead7ba484d12275ae52dc6c396275
dydyFos <dylanfoster.esgi@gmail.com>
2017-03-03 09:03:07 +0100

    yet another composer f*** up

--- module/Blog/src/Controller/IndexController.php
+++ module/Blog/src/Controller/IndexController.php
@@ -5,23 +5,38 @@ use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 use Blog\Form\Add;
 use Blog\InputFilter\AddPost;
+
+
 class IndexController extends AbstractActionController {
+
     protected $container;
+
     public function __construct($container) {
         $this->container = $container;
     }
+
     public function indexAction () {
         $variables = [
-            'name' => "quoimaggle"
+            'name' => 'quoi maggle'
         ];
         //@todo fetch blog pg_connection_status
         return new ViewModel($variables);
     }
     public function addAction () {
-        $form = new Add("fdp");
-        $variables = [
-            'form' => $form
-        ];
-        return new ViewModel($variables);
+      $form = new Add();
+      $variables = [
+          'form' => $form
+      ];
+      if ($this->request->isPost()) { //Si le form a été submit
+          $form->setInputFilter(new AddPost());
+          $data = $this->request->getPost(); //key value array
+          $form->setData($data);
+          if ($form->isValid()) {
+              // @todo insert into db
+              return $this->redirect()->toRoute('blog_home');
+          }
+      }
+      return new ViewModel($variables);
     }
+
 }
