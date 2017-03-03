3ead1a3e8ebf91b0040cd47d975236f78fb384fc
dydyFos <dylanfoster.esgi@gmail.com>
2017-03-03 11:01:53 +0100

    repository left to finalyy be able to F*** save in db with f**** zend

--- module/Blog/src/Controller/IndexController.php
+++ module/Blog/src/Controller/IndexController.php
@@ -5,14 +5,15 @@ use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 use Blog\Form\Add;
 use Blog\InputFilter\AddPost;
+use Blog\\Entity\Post;
 
 
 class IndexController extends AbstractActionController {
 
-    protected $container;
+    protected $blogService;
 
-    public function __construct($container) {
-        $this->container = $container;
+    public function __construct($blogService) {
+        $this->container = $blogService;
     }
 
     public function indexAction () {
@@ -28,10 +29,14 @@ class IndexController extends AbstractActionController {
           'form' => $form
       ];
       if ($this->request->isPost()) { //Si le form a été submit
+          $blogPost = new Post();
+          $form->bind($blogPost);
+
           $form->setInputFilter(new AddPost());
           $data = $this->request->getPost(); //key value array
           $form->setData($data);
           if ($form->isValid()) {
+              $this->blogService->save($blogPost);
               // @todo insert into db
               return $this->redirect()->toRoute('blog_home');
           }
