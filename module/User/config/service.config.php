bef8eab496a01eee4d55184127ca60ecabf3243e
dydyFos <dylanfoster.esgi@gmail.com>
2017-05-15 11:17:16 +0200

    AdduSer.php

--- module/User/config/service.config.php
+++ module/User/config/service.config.php
@@ -1,5 +1,9 @@
 <?php
 namespace User;
 return [
-
+    'factories' => [
+        'User\InputFilter\AddUser' => function(\Zend\ServiceManager\ServiceManager $sm) {
+            // $addUserFilter = new \User\InputFilter\AddUser
+        }
+    ]
 ];
