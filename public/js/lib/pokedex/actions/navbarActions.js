1e0c5a9630b978bc6575bf0f672818c1bcfacbfd
dydyFos <dylanfoster.esgi@gmail.com>
2017-06-09 23:03:25 +0200

    useless action

--- public/js/lib/pokedex/actions/navbarActions.js
+++ public/js/lib/pokedex/actions/navbarActions.js
@@ -8,24 +8,3 @@ export const toggleNavbar = () => {
         type: types.TOGGLE_DISPLAY
     }
 }
-
-export const testAction = () => {
-    return (dispatch, getState) => {
-        console.log("mother fucker bitch ", getState());
-        pokemonApi.getAll(response => {
-            console.debug("GetAll -- SERVER RESPONSE", response);
-        });
-        pokemonApi.get(1, response => {
-            console.debug("GET -- SERVER RESPONSE", response);
-        });
-        pokemonApi.create(undefined, response => {
-            console.debug("CREATE -- SERVER RESPONSE", response);
-        });
-        pokemonApi.update(undefined, undefined, response => {
-            console.debug("UPDATE -- SERVER RESPONSE", response);
-        });
-        pokemonApi.delete(undefined, response => {
-            console.debug("DELETE -- SERVER RESPONSE", response);
-        });
-    }
-}
