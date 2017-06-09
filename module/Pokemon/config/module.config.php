034167e1d901640408df27a2261340382bb78a30
dydyFos <dylanfoster.esgi@gmail.com>
2017-06-09 11:03:20 +0200

    display errors config in module.config.chibar

--- module/Pokemon/config/module.config.php
+++ module/Pokemon/config/module.config.php
@@ -75,5 +75,17 @@ return [
         'strategies' => [
             'ViewJsonStrategy',
         ],
+        'display_not_found_reason' => true,
+        'display_exceptions'       => true,
+        'doctype'                  => 'HTML5',
+        'not_found_template'       => 'error/404',
+        'exception_template'       => 'error/index',
+        'template_map' => [
+            'error/404'               => __DIR__ . '/../view/error/404.phtml',
+            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+        ],
+        'template_path_stack' => [
+            __DIR__ . '/../view',
+        ],
     ],
 ];
