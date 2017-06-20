cac399d14125c4756a6c6f77fbd6e02dc3824f86
tmeksava <teddy.meksavanh@arkena.com>
2017-06-20 10:51:34 +0200

    add block c to index

--- public/js/lib/pokedex/ui/Home.js
+++ public/js/lib/pokedex/ui/Home.js
@@ -3,8 +3,24 @@ import {PropTypes as T} from 'prop-types';
 import MapContainer from '../container/MapContainer';
 import Cards from "../container/Cards";
 import {Grid, Row, Col} from 'react-bootstrap';
+import RaisedButton from 'material-ui/RaisedButton';
+import FontIcon from 'material-ui/FontIcon';
 
 const styles = {
+  button: {
+    margin: 12,
+    width: 'auto'
+  },
+  exampleImageInput: {
+    cursor: 'pointer',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    width: '100%',
+    opacity: 0,
+  },
 };
 
 export default class Home extends React.PureComponent {
@@ -21,7 +37,38 @@ export default class Home extends React.PureComponent {
                   <Cards/>
                 </Col>
                 <Col md={6}>
-                  <div className="title-lg">
+                  <div className="title-lg-wrapper align">
+                    <div>
+                      <span className="title-lg">
+                        Le pokedex
+                      </span>
+                      <span className="title-lg">
+                        le plus complet
+                      </span>
+                      <span className="title-lg">
+                        et le plus rapide
+                      </span>
+                      <div className="text-center">
+                        <RaisedButton
+                          href="https://github.com/callemall/material-ui"
+                          target="_blank"
+                          label="Ajouter un pokémon"
+                          labelColor="#ffffff"
+                          backgroundColor="#a4c639"
+                          style={styles.button}
+                          icon={<FontIcon className="muidocs-icon-custom-github" />}
+                        />
+                        <RaisedButton
+                          href="https://github.com/callemall/material-ui"
+                          target="_blank"
+                          label="Voir la map"
+                          labelColor="#ffffff"
+                          secondary={true}
+                          style={styles.button}
+                          icon={<FontIcon className="muidocs-icon-custom-github" />}
+                        />
+                      </div>
+                    </div>
                   </div>
                 </Col>
                 <Col md={6}>
